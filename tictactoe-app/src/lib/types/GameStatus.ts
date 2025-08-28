ec9ab43b1183ddbf1a5fa876ed3b27f8c509ad1c
j <anonymosmail@gmail.com>
2025-08-28 09:03:21 +0200

    add: GameStatus and PlayerTypes

---
+++ tictactoe-app/src/lib/types/GameStatus.ts
@@ -0,0 +1,6 @@
+export type GameStatus = "MATCHMAKING" | "IN_PROGRESS" | "FINISHED";
+export const GameStatus = {
+  MATCHMAKING: "MATCHMAKING",
+  IN_PROGRESS: "IN_PROGRESS",
+  FINISHED: "FINISHED"
+};
